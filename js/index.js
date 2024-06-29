00cce84ccaaaecd0a825eb87180b19b3c9ee385e
unknown <dung.haquoc@gmail.com>
2024-06-29 23:24:51 +0700

    Ghichú-cho-Commit

--- js/index.js
+++ js/index.js
@@ -1,131 +1,104 @@
-import {NhanVien,stringToSlug} from "./method.js"
-let list = []
-function xepLoai(gio){
-    let loai = ''
-    if(gio >= 192){ loai = 'xuất sắc'}
-    else if(gio >= 176){ loai = 'giỏi'}
-         else if(gio >= 160){ loai = 'khá'}
-            else { loai = 'trung bình'}
-   return loai
-}
+import {
+  layDuLieu,
+  xuat,
+  reset,
+  stringToSlug,
+  kiemTraRong,
+  kiemTraDoDai,luong,xepLoai,kiemTraTen,kiemTraEmail,kiemTraMatKhau,kiemTraNgayLam,kiemTraLuong,kiemTraGioLam
+} from "./method.js";
+let list = [];
+document.querySelector("#btnThem").onclick = function (e) {
+  document.querySelector("#btnThemNV").style.display = "inline-block";
+  document.querySelector("#btnCapNhat").style.display = "none";
+  reset();
+};
 
-function luong(luongCb,chucVu){
-    let heSo = 1
- switch (chucVu){
-    case 'Giám đốc':{
-        heSo = 3
-    } break
-    case 'Trưởng phòng':{
-        heSo = 2
-    } break
- }
- return luongCb * heSo
-}
+document.getElementById("tknv").addEventListener("input", function (e) {
+  let tknv = document.querySelector("#tknv").value;
+  let kq = list.filter((sv) => sv.tknv === tknv);
+  if (kq.length > 0) {
+    alert("Tài khoản đã tồn tại");
+  }
+});
 
-function layDuLieu(){
-    let NV = new NhanVien()
-    let arrInput = document.querySelectorAll('form .form-control')
-    for(let input of arrInput){
-        let id = input.id
-        let value = input.value
-        NV[id] = value
+document.querySelector("#btnThemNV").onclick = function (e) {
+  let nv = layDuLieu();
+  console.log(nv.tknv, "tknv");
+  // let {tknv,name,email,password,datepicker,luongCB,chucVu,gioLam} = tam;
+  let valid = true;
+  for (let tt in nv) {
+    valid &= kiemTraRong(nv[tt], tt);
+  }
+  valid &= kiemTraDoDai(nv.tknv,'tknv') 
+  & kiemTraTen(nv.name,'name') 
+  & kiemTraEmail(nv.email,'email')
+  & kiemTraMatKhau(nv.password,'password')
+  & kiemTraNgayLam(nv.datepicker,'datepicker')
+  & kiemTraLuong(nv.luongCB,'luongCB')
+  & kiemTraGioLam(nv.gioLam,'gioLam')
+  if(!valid){
+    return;
+}
+  nv.tongLuong = luong(nv.luongCB, nv.chucVu);
+  nv.xepLoai = xepLoai(nv.gioLam);
+  list.push(nv);
+  reset();
+  xuat(list);
+};
+
+document.querySelector("#btnCapNhat").onclick = function (e) {
+  $("#myModal").modal("hide");
+  document.querySelector("#tknv").disabled = false;
+  let tam = layDuLieu();
+  for (let i in list) {
+    if (list[i].tknv === tam.tknv) {
+      list[i] = tam;
     }
-    NV.tongLuong = luong(NV.luongCB,NV.chucVu)
-    NV.xepLoai = xepLoai(NV.gioLam)
-    return NV
-}
+  }
+  tam.tongLuong = luong(tam.luongCB, tam.chucVu);
+  tam.xepLoai = xepLoai(tam.gioLam);
+  xuat(list);
+};
 
-function xuat(ds){
-    let htmlStr = ''
-    for(let nv of ds){
-        htmlStr += `
-  <tr>
-    <th>${nv.tknv}</th>
-    <th>${nv.name}</th>
-    <th>${nv.email}</th>
-    <th>${nv.datepicker}</th>
-    <th>${nv.chucVu}</th>									
-    <th>${nv.tongLuong}</th>
-    <th>${nv.xepLoai}</th>
-    <th><button type="button" class="btn btn-success" onclick="edit('${nv.tknv}')"  style="cursor:pointer">Edit</button>
-    <button type="button" class="btn btn-success" style="cursor:pointer" onclick="del('${nv.tknv}')">Delete</button></th>
- </tr>`}
-    document.querySelector('tbody').innerHTML = htmlStr
-}
+document.querySelector("#btnDong").onclick = function (e) {
+  document.querySelector("#tknv").disabled = false;
+  xuat(list);
+};
 
-window.del = function (ma){
-    let indexDel = list.findIndex(sv => sv.tknv === ma);
-    if(indexDel !== -1)
-   list.splice(indexDel,1)
-    xuat(list)
-}
+document.querySelector("#btnTimNV").onclick = function (e) {
+  let tam = document.querySelector("#searchName").value;
+  tam = stringToSlug(tam);
+  let kq = list.filter((sv) => stringToSlug(sv.xepLoai).search(tam) !== -1);
+  if (kq) {
+    xuat(kq);
+  }
+};
+
+searchName.addEventListener("input", function () {
+  if (searchName.value == "") {
+    xuat(list);
+  }
+});
+
+xuat(list);
+
+window.del = function (ma) {
+  let indexDel = list.findIndex((sv) => sv.tknv === ma);
+  if (indexDel !== -1) list.splice(indexDel, 1);
+  xuat(list);
+};
 
 window.edit = function (ma) {
-    $('#myModal').modal('show')
-    document.querySelector('#btnThemNV').style.display = 'none'
-    document.querySelector('#btnCapNhat').style.display = 'inline-block'
-    document.querySelector('#tknv').disabled = true
-    let svUpdate = list.find(sv => sv.tknv === ma);
-    if(svUpdate) {
-        for (let key in svUpdate){
-            document.querySelector(`#${key}`).value = svUpdate[key]
-        }
+  $("#myModal").modal("show");
+  document.querySelector("#btnThemNV").style.display = "none";
+  document.querySelector("#btnCapNhat").style.display = "inline-block";
+  document.querySelector("#tknv").disabled = true;
+  let svUpdate = list.find((sv) => sv.tknv === ma);
+  if (svUpdate) {
+    for (let key in svUpdate) {
+      document.querySelector(`#${key}`).value = svUpdate[key];
     }
-}
-
-function reset(){
-    let arrInput = document.querySelectorAll('form .form-control')
-    for(let input of arrInput){
-        input.value = ''
-}}
-
-document.querySelector('#btnThem').onclick = function(e){
-    document.querySelector('#btnThemNV').style.display = 'inline-block'
-    document.querySelector('#btnCapNhat').style.display = 'none'
-    reset()
-}
-
-document.getElementById('tknv').addEventListener('input', function(e) {
-    let tknv = document.querySelector('#tknv').value
-    let kq = list.filter(sv => sv.tknv === tknv);
-    if (kq.length > 0) {alert('Tài khoản đã tồn tại')} 
-    })
-
-document.querySelector('#btnThemNV').onclick = function(e){
-    let tam = layDuLieu()
-    list.push(tam)
-    reset()
-    xuat(list)
-}
-
-document.querySelector('#btnCapNhat').onclick = function(e){
-    $('#myModal').modal('hide')
-    document.querySelector('#tknv').disabled = false
-    let tam = layDuLieu()
-    for(let i in list){
-        if(list[i].tknv === tam.tknv){
-            list[i] = tam
-        }
-    }
-    xuat(list)
-    
-}
-
-document.querySelector('#btnDong').onclick = function(e){
-    document.querySelector('#tknv').disabled = false
-    xuat(list)
-}
-
-document.querySelector('#btnTimNV').onclick = function(e){
-    let tam = document.querySelector('#searchName').value
-    tam = stringToSlug(tam)
-    let kq = list.filter(sv => stringToSlug(sv.xepLoai).search(tam) !== -1);
-    if (kq){ xuat(kq)}
-}
-
-searchName.addEventListener('input', function() { 
-    if(searchName.value == ''){
-xuat(list)}
-})
-
-xuat(list)
+  }
+};
+// let xy= 'tknv'
+// document.getElementById(`tb${xy}`).innerHTML = 'không có ji'
--- js/method.js
+++ js/method.js
@@ -1,40 +1,214 @@
 export class NhanVien {
-    tknv = ''
-    name = ''
-    email = ''
-    password = ''
-    datepicker = ''
-    luongCB = ''
-    chucVu = ''
-    gioLam = ''
-    tongLuong = ''
-    xepLoai = ''
+  tknv = "";
+  name = "";
+  email = "";
+  password = "";
+  datepicker = "";
+  luongCB = "";
+  chucVu = "";
+  gioLam = "";
+
 }
 export function stringToSlug(title) {
-    //Đổi chữ hoa thành chữ thường
-   let slug = title.toLowerCase();
-    //Đổi ký tự có dấu thành không dấu
-    slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a');
-    slug = slug.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e');
-    slug = slug.replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i');
-    slug = slug.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o');
-    slug = slug.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u');
-    slug = slug.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y');
-    slug = slug.replace(/đ/gi, 'd');
-    //Xóa các ký tự đặt biệt
-    slug = slug.replace(/\`|\~|\!|\@|\#|\||\$|\%|\^|\&|\*|\(|\)|\+|\=|\,|\.|\/|\?|\>|\<|\'|\"|\:|\;|_/gi, '');
-    //Đổi khoảng trắng thành ký tự gạch ngang
-    slug = slug.replace(/ /gi, "-");
-    //Đổi nhiều ký tự gạch ngang liên tiếp thành 1 ký tự gạch ngang
-    //Phòng trường hợp người nhập vào quá nhiều ký tự trắng
-    slug = slug.replace(/\-\-\-\-\-/gi, '-');
-    slug = slug.replace(/\-\-\-\-/gi, '-');
-    slug = slug.replace(/\-\-\-/gi, '-');
-    slug = slug.replace(/\-\-/gi, '-');
-    //Xóa các ký tự gạch ngang ở đầu và cuối
-    slug = '@' + slug + '@';
-    slug = slug.replace(/\@\-|\-\@|\@/gi, '');
-
-    return slug;
+  //Đổi chữ hoa thành chữ thường
+  let slug = title.toLowerCase();
+  //Đổi ký tự có dấu thành không dấu
+  slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, "a");
+  slug = slug.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, "e");
+  slug = slug.replace(/i|í|ì|ỉ|ĩ|ị/gi, "i");
+  slug = slug.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, "o");
+  slug = slug.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, "u");
+  slug = slug.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, "y");
+  slug = slug.replace(/đ/gi, "d");
+  //Xóa các ký tự đặt biệt
+  slug = slug.replace(
+    /\`|\~|\!|\@|\#|\||\$|\%|\^|\&|\*|\(|\)|\+|\=|\,|\.|\/|\?|\>|\<|\'|\"|\:|\;|_/gi,
+    ""
+  );
+  //Đổi khoảng trắng thành ký tự gạch ngang
+  slug = slug.replace(/ /gi, "-");
+  //Đổi nhiều ký tự gạch ngang liên tiếp thành 1 ký tự gạch ngang
+  //Phòng trường hợp người nhập vào quá nhiều ký tự trắng
+  slug = slug.replace(/\-\-\-\-\-/gi, "-");
+  slug = slug.replace(/\-\-\-\-/gi, "-");
+  slug = slug.replace(/\-\-\-/gi, "-");
+  slug = slug.replace(/\-\-/gi, "-");
+  //Xóa các ký tự gạch ngang ở đầu và cuối
+  slug = "@" + slug + "@";
+  slug = slug.replace(/\@\-|\-\@|\@/gi, "");
+  return slug;
 }
 
+export function xepLoai(gio) {
+  let loai = "";
+  if (gio >= 192) {
+    loai = "xuất sắc";
+  } else if (gio >= 176) {
+    loai = "giỏi";
+  } else if (gio >= 160) {
+    loai = "khá";
+  } else {
+    loai = "trung bình";
+  }
+  return loai;
+}
+
+export function luong(luongCb, chucVu) {
+  let heSo = 1;
+  switch (chucVu) {
+    case "Giám đốc":
+      {
+        heSo = 3;
+      }
+      break;
+    case "Trưởng phòng":
+      {
+        heSo = 2;
+      }
+      break;
+  }
+  return luongCb * heSo;
+}
+
+export function layDuLieu() {
+  let NV = {}
+  let arrInput = document.querySelectorAll("form .form-control");
+  for (let input of arrInput) {
+    let id = input.id;
+    let value = input.value;
+    NV[id] = value;
+  }
+//   NV.tongLuong = luong(NV.luongCB, NV.chucVu);
+//   NV.xepLoai = xepLoai(NV.gioLam);
+  return NV;
+}
+
+export function xuat(ds) {
+  let htmlStr = "";
+  for (let nv of ds) {
+    htmlStr += `
+  <tr>
+    <th>${nv.tknv}</th>
+    <th>${nv.name}</th>
+    <th>${nv.email}</th>
+    <th>${nv.datepicker}</th>
+    <th>${nv.chucVu}</th>									
+    <th>${nv.tongLuong}</th>
+    <th>${nv.xepLoai}</th>
+    <th><button type="button" class="btn btn-success" onclick="edit('${nv.tknv}')"  style="cursor:pointer">Edit</button>
+    <button type="button" class="btn btn-success" style="cursor:pointer" onclick="del('${nv.tknv}')">Delete</button></th>
+ </tr>`;
+  }
+  document.querySelector("tbody").innerHTML = htmlStr;
+}
+
+export function reset() {
+  let arrInput = document.querySelectorAll("form .form-control");
+  for (let input of arrInput) {
+    input.value = "";
+  }
+}
+
+export function kiemTraRong(value, selectorError) {
+  let tb = document.getElementById(`tb-${selectorError}`);
+  let nd = document.getElementById(`${selectorError}`);
+  if (value.trim() === "") {
+    tb.style.display = "block";
+    tb.innerHTML = `${nd.getAttribute("placeholder")} không được bỏ trống !`;
+    return false;
+  }
+  tb.style.display = "none";
+  return true;
+}
+
+export function kiemTraDoDai(value, selectorError) {
+  let length = value.length;
+  let tb = document.getElementById(`tb-${selectorError}`);
+  let nd = document.getElementById(`${selectorError}`);
+  if (length > 6 || length < 4) {
+    tb.style.display = "block";
+    tb.innerHTML = `${nd.getAttribute("placeholder")} từ 4-6 ký số !`;
+    return false;
+  }
+  tb.style.display = "none";
+  return true;
+}
+
+export function kiemTraTen(value,selectorError) {
+    let regexName = /^([A-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơỲỴÝỳỵỷỹĂăẠ-ỹ]+(\s[A-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơỲỴÝỳỵỷỹĂăẠ-ỹ]+)*)$/ig;
+    let tb = document.getElementById(`tb-${selectorError}`);
+    let nd = document.getElementById(`${selectorError}`);
+    if(regexName.test(value)){
+        tb.style.display = "none";
+        return true;
+    }
+    tb.style.display = "block";
+    tb.innerHTML = `${nd.getAttribute("placeholder")} phải là chữ !`;
+    return false;
+}
+
+export function kiemTraEmail(value,selectorError) {
+    let regexEmail = /^[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,}$/;
+    let tb = document.getElementById(`tb-${selectorError}`);
+    let nd = document.getElementById(`${selectorError}`);
+    if(regexEmail.test(value)){
+        tb.style.display = "none";
+        return true;
+    }
+    tb.style.display = "block";
+    tb.innerHTML = `${nd.getAttribute("placeholder")} không đúng định dạng !`;  
+    return false;
+}
+
+export function kiemTraMatKhau(value,selectorError) {
+    let length = value.length;
+    let regexMatKhau = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,10}$/;
+    let tb = document.getElementById(`tb-${selectorError}`);
+    let nd = document.getElementById(`${selectorError}`);
+    if((regexMatKhau.test(value))&&(length <= 10 && length >=6)){
+        tb.style.display = "none";
+        return true;
+    }
+    tb.style.display = "block";
+    tb.innerHTML = `${nd.getAttribute("placeholder")} từ 6-10 ký tự (chứa ít nhất 1 ký tự số, 1 ký tự in hoa, 1 ký tự đặc biệt) !`;
+    return false;
+}
+
+export function kiemTraNgayLam(value,selectorError) {
+    let regexNgayLam = /^(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])\/(19|20)\d{2}$/;
+    let tb = document.getElementById(`tb-${selectorError}`);
+    let nd = document.getElementById(`${selectorError}`);
+    if(regexNgayLam.test(value)){
+        tb.style.display = "none";
+        return true;
+    }
+    tb.style.display = "block";
+    tb.innerHTML = `${nd.getAttribute("placeholder")} phải là định dạng dd/mm/yyyy !`;
+    return false;
+}
+
+export function kiemTraLuong(value,selectorError) {
+    let tb = document.getElementById(`tb-${selectorError}`);
+    let nd = document.getElementById(`${selectorError}`);
+    if(value > 20000000 || value < 1000000) {
+    tb.style.display = "block";
+    tb.innerHTML = `${nd.getAttribute("placeholder")} phải có giá trị nhập từ 1.000.000 - 20.000.000 !`;
+        return false;
+    }
+    tb.style.display = "none";;
+    return true;
+
+}
+
+export function kiemTraGioLam(value,selectorError) {
+    let tb = document.getElementById(`tb-${selectorError}`);
+    let nd = document.getElementById(`${selectorError}`);
+    if(value > 200 || value < 80) {
+    tb.style.display = "block";
+    tb.innerHTML = `${nd.getAttribute("placeholder")} phải có giá trị nhập từ 80 - 200 !`;
+        return false;
+    }
+    tb.style.display = "none";;
+    return true;
+
+}
